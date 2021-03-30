ced952ce0e254b0467439e81ea9ff150901e3f7b
hachiojidev <hachiojidev@protonmail.com>
2021-03-30 16:56:43 +0900

    fix: Get bet hook with no account

--- src/state/hooks.ts
+++ src/state/hooks.ts
@@ -296,6 +296,11 @@ export const useGetMinBetAmount = () => {
 
 export const useGetUserBetByRound = (id: string, account: string): Bet => {
   const round = useGetRound(id)
+
+  if (!account) {
+    return undefined
+  }
+
   return round.bets.find((bet) => bet.user.address.toLowerCase() === account.toLocaleLowerCase())
 }
 
